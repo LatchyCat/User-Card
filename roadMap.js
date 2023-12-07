a4294f73e92a0cd635105945a9b8514b128088ea
oriel <orielsilvajr@gmail.com>
2023-12-07 16:03:51 -0500

    lot of work

--- roadMap.js
+++ roadMap.js
@@ -6,11 +6,19 @@ Div's to create: 4
 1. Create main container
 2. Create four separate div's (topNav = 1div, middleNav = 2div, bottomNav = 1)
 
+
+
 html: class ="navBarTop"
 work: buttons = 4, type='text' = 1, textarea = 1 (includes a image of a small magnify glass within the textarea)
 
+
+
+
 html: class ="middleNavTop"
 work: back-ground: blue; justify-content: center;
+
+
+
 
 html: class ="middleNavBottom", class ='profileName
 html code pseudo: <h1 class ='profileName> Oriel Silva <h1/>,
@@ -19,6 +27,8 @@ html code pseudo: <p id="jobStatus"> Developer | Engineer | Problem Solver </p>
 html code pseudo: <p id="jobStatus"> Master of shadows and the Internet! </p>
 html code pseudo: <link rel="icon" href="random page"> edit profile </link>
 work: back-ground: white; justify-content: center;
+
+
 
 html: class ="bottomNav"
 work: small image of two line shifted 85 degrees upWard from bottom right corner of page
